0d23a0647c6a030239b06034fbc27ce91ae4c33a
Asya Seagrave <asyaseagrave@gmail.com>
2025-10-19 00:25:07 +0100

    upd for deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from "react";
-import "./index.css";
+import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Projects from "./routes/Projects";
 import Contact from "./routes/Contact";
 
-import { HashRouter, Route, Routes } from "react-router-dom";
-
-
 function App() {
   return (
-    <>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </HashRouter>
-
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
   );
 }
 
