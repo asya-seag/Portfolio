9f2e672cf3c30f56ad5d04df602b9c06933319d9
Asya Seagrave <asyaseagrave@gmail.com>
2025-10-19 01:36:53 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -9,14 +9,12 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </>
   );
 }
 
