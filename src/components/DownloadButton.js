6ca9fd0823c8f3190c545a1b06833c3ae22d4d86
Asya Seagrave <asyaseagrave@gmail.com>
2025-10-20 11:37:15 +0100

    getting download to work

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -3,11 +3,20 @@ import "./DownloadButton.css";
 
 const DownloadButton = ({ text, file }) => {
   return (
-    <a href={file} download className="arrow-button-d">
+     <a
+      href={process.env.PUBLIC_URL + file}
+      download
+      className="arrow-button-d"
+    >
       <span className="text-arrow-d">{text}</span>
       <span className="arrow-d">&rarr;</span>
     </a>
   );
 };
 
-export default DownloadButton;
+
+
+
+
+
+   
