197eedb620bcb2a9075473166ae359c3d3c69352
Asya Seagrave <asyaseagrave@gmail.com>
2025-10-20 11:43:52 +0100

    upd

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -4,10 +4,11 @@ import "./DownloadButton.css";
 const DownloadButton = ({ text, file }) => {
   return (
      <a
-      href={process.env.PUBLIC_URL + file}
+      href={`${process.env.PUBLIC_URL}${file}`}
       download
       className="arrow-button-d"
     >
+      
       <span className="text-arrow-d">{text}</span>
       <span className="arrow-d">&rarr;</span>
     </a>
